b9a5dca21024f396746210f41c55c2201f761f01
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-03-23 08:55:01 -0600

    ya se puede remover la fruta final

--- assets/js/frutas.js
+++ assets/js/frutas.js
@@ -1,6 +1,6 @@
 var boton = document.getElementById('boton'); // id del boton
 var listaOrdenada= document.getElementById('lista');// id de la lista ordenada
-
+var botonRemove = document.getElementById('botonRemove');
 
 function agregarFruta(evento){
   evento.preventDefault(); // esto hace que la pagina no se refresque y no se pierda la info
@@ -10,3 +10,10 @@ function agregarFruta(evento){
   listaOrdenada.appendChild(li); //<ol> <li> innerHTML </li></ol>
 }
 boton.addEventListener('click', agregarFruta); // cuando aprietan el boton agrega la info de la fucncion agregar Fruta
+
+function quitarFruta(){
+  var item = document.getElementsByTagName('li');
+  var i = item.length-1;
+  listaOrdenada.removeChild([i]);
+}
+botonRemove.addEventListener('click', quitarFruta);
